3345d2563cd554cd3d4190ced9b71261e37e71ed
aglotrex <aglotrex@users.noreply.github.com>
2018-11-08 01:09:22 +0100

    Add files via upload

---
+++ src/Request/RequestP.java
@@ -0,0 +1,43 @@
+package Request;
+
+public class RequestP
+implements RequestIP
+{
+	private String uriRQ;
+	private String uri_value;
+	private long value;
+	private RequestType type;
+
+    public RequestP(String uriRQ, String uri_value, long value, RequestType type) {
+        this.uriRQ = uriRQ;
+        this.uri_value = uri_value;
+        this.value = value;
+        this.type = type;
+    }
+    public RequestP(String uri_value, long value, RequestType type) {
+        this.uriRQ = java.util.UUID.randomUUID().toString();
+        this.uri_value = uri_value;
+        this.value = value;
+        this.type = type;
+    }
+
+    @Override
+	public String getRequestURI() {
+		return uriRQ;
+	}
+
+	@Override
+	public String getURI() {
+		return uri_value;
+	}
+
+	@Override
+	public long getValue() {
+		return value;
+	}
+
+	@Override
+	public RequestType getType() {
+		return type;
+	}
+}
